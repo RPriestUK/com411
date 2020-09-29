6bc9a77bf13d008f893399ff699021a44582fb57
repl.it user <replituser@example.com>
2020-09-29 10:42:19 +0000

    Basics - inputs - added dynamic eyes to Beep the robot

---
+++ 1-basics/1-input/ascii_robot.py
@@ -0,0 +1,18 @@
+#Ask user to choose eye character for Beep
+print("Please choose a character for Beep's eyes")
+eye = input()
+
+# Display a picture of Beep the robot
+print("\n")
+print("   ###########")
+print("# # ( " + eye + " / " + eye +" ) # #")
+print("# #      /    # #")
+print("   ###########")
+print("\t  #####")
+print("   ###########")
+print("  #  # --- #  #")
+print(" #   #o..  #   #")
+print(" #   #     #   #")
+print("  #  #     #  #")
+print("   ###########")
+print("\n")
