14929bce868723d206b99e682d5148a2d34bb673
repl.it user <replituser@example.com>
2020-09-29 10:52:31 +0000

    Tidying files and adding data types

---
+++ basics/input/data_types.py
@@ -0,0 +1,12 @@
+print("What is your name, human?")
+name = input(str())
+print("How old are you, in years?")
+age = input(int())
+print("How tall are you, in metres?")
+height = input(float())
+print("How much do you weigh, in kilograms?")
+weight = input(int())
+
+bmi = weight / (height ** 2)
+
+print("bmi")
