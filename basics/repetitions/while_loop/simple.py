cde4e8b64ec122f407f9e591f1c5d1a27d0d9092
repl.it user <replituser@example.com>
2020-10-06 17:22:10 +0000

    Basics - Part 9 - Activity 1 - Simple While Loop

---
+++ basics/repetitions/while_loop/simple.py
@@ -0,0 +1,16 @@
+# The program should start by displaying the message "How many cables should I remove?".
+# The program should then read in the user's response.
+# The program should then create a variable to track the number of removed cables and set this to 0.
+# The program should then use a while loop to do the following:
+#    Display the message "Removed cable."
+#    Increment the variable for tracking the number of removed cables
+
+print("How many cables should I remove?")
+n = int(input())
+
+# number of cables removed
+i = 0
+
+while (i < n):
+  print("Removed cable.")
+  i = i + 1
