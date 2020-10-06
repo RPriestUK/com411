cf04fb6c70cc0490ab82cca2cf0abd9152951bba
repl.it user <replituser@example.com>
2020-10-06 11:04:13 +0000

    Basics - ifs part 6 - formatting output of odds/evens - amending if.py

--- basics/decisions/simple_decisions/if.py
+++ basics/decisions/simple_decisions/if.py
@@ -8,6 +8,7 @@
 print("What type of book do you have?")
 booktype = str(input())
 
-print("I like {} books!".format(booktype))
+if (booktype == "adventure"):
+  print("I like {} books!".format(booktype))
 
 print("I've finished reading that book now, very interesting!")
