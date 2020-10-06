6e9d253687663bc3889bfe2266bb76f6723d2408
repl.it user <replituser@example.com>
2020-10-06 18:53:30 +0000

    Basics - Part 10 - Activity 2 - Countdown for loop

---
+++ basics/repetitions/for_loop/count_down.py
@@ -0,0 +1,13 @@
+# We wish to create a program that counts down the distance (in number of steps) to the cave.
+
+# The program should begin by displaying the message "How far are we from the cave?".
+# The program should then read in the user's response (a distance)
+# Finally, the program should use a for loop to display the number of steps remaining.
+
+print("How far are we from the cave?")
+n = int(input())
+print()
+
+for count in range(n, 0, -1):
+  print("{} steps remaining".format(n))
+  n = n - 1
