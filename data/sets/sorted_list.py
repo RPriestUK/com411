1630c59b5c81b761167d57a077445d4b3991dc8c
repl.it user <replituser@example.com>
2020-10-27 12:50:15 +0000

    Lec 5 - Act 3 - Sorted List Sets

--- data/sets/sorted_list.py
+++ data/sets/sorted_list.py
@@ -18,32 +18,57 @@
 # Finally, the function should display a sorted set of the observations and how many times each observation has been made.
 
 def observed():
-  # define empty set
-  Observations = set()
-  # loop for 7 input requests
+  # define empty list
+  observations = []
+  # loop for 5 input requests
   for count in range(5):
-    # Ask user for objects they have observed and add to set
+    # Ask user for objects they have observed and add to list
     print("Please enter an observation:")
-    Observations.add(input())
-  return Observations
+    observations.append(input())
+  return observations
 
 def remove_observations(observations):
-  print("Are you sure you wish to delete an observation?")
-  confirmation = str(input())
-  while confirmation == "y":
-    if confirmation == "y":
+  # Initialise while loop
+  is_running = True
+  # ask for observation to remove while user has positively confirmed.
+  while(is_running):
+    # Get user's response
+    print("Are you sure you wish to delete an observation? y/n")
+    confirmation = str(input())
+    # If user response is y, then remove from set, otherwise exit
+    if (confirmation == "y"):
       # Request which observation to remove
       print("Please enter an observation to remove:")
       ob_to_rem = str(input())
       # discard entry if matching the input
-      if (ob_to_rem == ""):
+      if (ob_to_rem == observations):
         # Remove observation
         observations.discard(ob_to_rem)
     else: 
-      # leave observations
-      print("Removal cancelled.")
+      # exit loop
+      is_running = False
 
+# set observations to local var, run remove
 def run():
-  Observations = set()
-  Observations(observed())
-  remove_observations(Observations)
+
+
+  # Display sorted set after removals
+  # assign to local set 
+  observations = observed()
+  remove_observations(observations)
+  # Initialise new set
+  observations_set = set()
+
+  # count number of each instance entered
+  for observation in observations:
+    # count the number of each type in observations set
+    num = observations.count(observation)
+    observations_set.add((observation, num))
+
+  # Display sorted set
+  for key, value in sorted(observations_set):
+    print("{} observed {} times.".format(key, value))
+  
+  
+# Start program
+run() 
