25008bc3dd6aff55a6352df964e02aefacd44662
repl.it user <replituser@example.com>
2020-11-03 08:22:46 +0000

    Data - 2710 - Act 3 - Sets

--- data/sets/set_from_list.py
+++ data/sets/set_from_list.py
@@ -1,22 +1,39 @@
+# We wish to create a program to help Beep and Bop record how many of each type of item they saw (e.g. 2 skyscrapers, 5 neon signs, etc.).
+
+# The program should consist of the following two functions:
+# The first function should be named observed and should have no parameters.
+# The function should create a list named observations.
+# The function should populate the list with 7 values read in from the user.  There should be some duplicate values.
+# Finally, the function should return the list named observations.
+# The second function should be named run and should have no parameters.
+# The function should start by displaying the message "Counting observations...".
+# The function should then call the first function and store the returned list in a local variable.
+#The function should then create a set that contains a tuple for each unique value in the list along with a count for how many times that value appeared in the list e.g. ("Skyscraper", 2)
+# Finally, the function should display the content of the set.
+
 def observed():
-  # define empty set
-  Observations = set()
+  # define empty list
+  observations = []
   # loop for 7 input requests
   for count in range(7):
-    # Ask user for objects they have observed and add to set
+    # Ask user for objects they have observed and add to list
     print("Please enter an observation:")
-    Observations.add(input())
-  return Observations
+    observations.append(input())
+  return observations
 
 def run():
   print("Counting observations...")
-  # assign to local set 'seen'
+  # assign to local set 
   observations = observed()
   observations_set = set()
-
   # count number of each instance entered
   for observation in observations:
-    observations_set.add((observation,observations.count(observation)))
+    # count the number of each type in observations set
+    num = observations.count(observation)
+    observations_set.add((observation, num))
+  # Display number
+  for key, value in observations_set:
+    print("{} observed {} times.".format(key, value))
+  
 
-  print(observations_set)
 run()
--- data/sets/simple_set.py
+++ data/sets/simple_set.py
@@ -1,8 +1,22 @@
+# We wish to create a program to help Beep and Bop record what they can see in the distance.
+
+# The program should consist of the following two functions:
+
+# The first function should be named observed and should have no parameters.
+# The function should create a set named observations.
+# The function should populate the list with the following items: "Flying Car", "Sky Scraper", "Laser" and "Dome". 
+# Finally, the function should return the set observations.
+# The second function should be named run and should have no parameters. 
+# The function should call the first function and display the set returned by the call.
+
+
 def observed():
-  observations = {"Flying Car", "Sky Scraper", "Laser", "Dome"}
+  # Create set with observations
+  observations = set({"Flying Car", "Sky Scraper", "Laser", "Dome"})
   return observations
 
 def run():
+  # Display result
   print(observed())
 
 run()
