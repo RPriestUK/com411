a81e923328c4a1bd7afcd561da27444078b87927
repl.it user <replituser@example.com>
2020-11-24 09:03:45 +0000

    2411 - Visualisation - 27 - Act 3 - Multiple real-world data plotting

--- visual/subplots/titanic.py
+++ visual/subplots/titanic.py
@@ -21,102 +21,199 @@
 # What interesting observations can be made?
 
 import matplotlib.pyplot as plt
-import numpy as np
 import csv
 
 def read_data():
-  # The function should read the file visual/subplots/titanic.csv and store its content into a dictionary so that the dictionary contains the following key-value pairs
-  data = {}
-  # Provide application with list of expected data (or blank list)
-  listOfValidItems = ['Survived', 'Sex', 'Age', 'Fare'] # accepted data items
-  # initialise other variables
-  validColumns = []
-  headerID = 0
-  rowID = 0  
+  # as data input is fixed, we can specify the keys when creating the dictionary, rather than gather dynamically
+  data = {'survived':[], 'sex':[], 'age':[], 'fare':[]}
 
-  # read from CSV file, check for valid columns and add the relevant data.
-  with open("visual/subplots/titanic.csv") as csvFile:
-    csv_reader = csv.reader(csvFile, delimiter=',', quotechar='"')
-    # check each row for data, on first row - create keys, else append data to appropriate key
-    for row in csv_reader:
-      if (rowID == 0): # add headers
-        for header in row:
-          # tidy up header value 
-          headerName = header.strip()
-          # initialise new key with empty list value
-          if (headerName in listOfValidItems):
-            data[headerName] = [] # adds key to data dictionary
-            validColumns.append((headerID, headerName)) # adds tuple to list of valid columns
-          # If list is empty, then add all columns
-          elif (listOfValidItems == []):
-            data[headerName] = [] # adds key to data dictionary
-            validColumns.append((headerID, headerName)) # adds tuple to list of valid columns
-          else:
-            pass #ignore
-          headerID += 1
-      else:
-        # loop through columns to add values to each key
-        columnID = 0
-        skipRow = 0
-        for item in row:
-          # check for invalid (blank) data before processing
-          if (item is None):
-            skipRow += 1
-          if (skipRow == 0):
-            # loop through each validColumn to retrieve column referencing
-            for index in range(0, len(validColumns), 1):
-              # Retrieve each column from validColumns for each index location
-              validColumn = validColumns[index]
-              # Check current column matches valid column and use tuple matching relevant key in data dictionary
-              if (validColumn[0] == columnID):
-                for key in data:
-                  # if key name matches column name, add data to key
-                  if (key == validColumn[1]):
-                      data[key].append(item.strip())
-          columnID += 1
-      rowID += 1
-    csvFile.close() # Closes file to prevent locking and performance issues. 
+  # open file and process into each key
+  with open('visual/subplots/titanic.csv') as file:
+    csv_reader = csv.reader(file)
+
+    # ignore header row by skipping 
+    header = next(csv_reader)
+
+    for line in csv_reader:
+      # extract required values using appropriate indexes
+      survived = line[1].strip()
+      sex = line[4].strip()
+      age = line[5].strip()
+      fare = line[9].strip()  
+
+      # only add the data to the dictionary if not empty
+      if (survived != "" and sex != "" and age != "" and fare != ""):
+        data['survived'].append(bool(int(survived)))
+
+        # split data according to gender
+        if (sex == 'male'): 
+          data['sex'].append('male')
+        else:
+          data['sex'].append('female')
+        
+        # convert numerical data to floats and round to 2 dp where required
+        data['age'].append(float(age))
+        data['fare'].append(round(float(fare), 2))
+
   return data
 
+def plot_age_vs_survival(ax, data):
+  # create dictionaries for each age group
+  children = {'survived':[], 'deceased':[]} # < 18
+  adults = {'survived':[], 'deceased':[]} # 18 -> 65
+  elderly = {'survived':[], 'deceased':[]} # 65+
+
+  # populate each dictionary
+  for index in range(len(data['age'])):
+    age = data['age'][index]
+    survived = data['survived'][index]
+
+    # add to relevant dictionary according to age and survival
+    if (age < 18 and survived):
+      children['survived'].append(age)
+    elif (age < 18 and not survived):
+      children['deceased'].append(age)
+    elif (age < 65 and survived):
+      adults['survived'].append(age)
+    elif (age < 65 and not survived):
+      adults['deceased'].append(age)
+    elif (survived == True):
+      elderly['survived'].append(age)
+    else:
+      elderly['deceased'].append(age)
+  
+  # prepare labels and totals
+  labels = ['children', 'adults', 'elderly']
+  survivors = [len(children['survived']), len(adults['survived']), len(elderly['survived'])]
+  deceased = [len(children['deceased']), len(adults['deceased']), len(elderly['deceased'])]
+
+  # display suitable bar plots
+  ax.bar(labels, survivors, label='Survived')
+  ax.bar(labels, deceased, bottom=survivors, label='Deceased') # labels, total, y coordinates of bar base
+  ax.set_ylabel('age')
+  ax.legend() # adds legend for survived/deceased on bar
+  ax.set_title('Age vs Survival')
+
+
+def plot_fare_vs_survival(ax, data):
+  survived = []
+  deceased = []
+
+  for index in range(len(data['fare'])):
+    fare = data['fare'][index]
+    # append survival boolean to relevant list. 
+    if (data['survived'][index]):
+      survived.append(fare)
+    else:
+      deceased.append(fare)
+  
+  ax.scatter(range(len(survived)), survived, label='Survived')
+  ax.scatter(range(len(deceased)), deceased, label='Deceased')
+  ax.set_ylabel('fare')
+  ax.legend()
+  ax.set_title('Fare vs Survival')
+
+def plot_sex_vs_survival(ax, data):
+  males = {'survived':[], 'deceased':[]}
+  females = {'survived':[], 'deceased':[]}
+
+  for index in range(len(data['sex'])):
+    sex = data['sex'][index]
+    if (sex == 'male' and data['survived'][index]):
+      males['survived'].append(sex)
+    elif (sex == 'male' and not data['survived'][index]):
+      males['deceased'].append(sex)
+    elif (sex == 'female' and data['survived'][index]):
+      females['survived'].append(sex)
+    else:
+      females['deceased'].append(sex)
+  
+  labels = ['male', 'female']
+  survivors = [len(males['survived']), len(females['survived'])]
+  deceased = [len(males['deceased']), len(females['deceased'])]
+
+  ax.bar(labels, survivors, label='Survived')
+  ax.bar(labels, deceased, bottom=survivors, label='Deceased')
+  ax.set_ylabel('passengers')
+  ax.legend()
+  ax.set_title('Sex vs Survival')
+
+
+def plot_all_vs_survival(ax, data):
+  children = {
+    'male': {'survived':[], 'deceased':[]},
+    'female': {'survived':[], 'deceased':[]}
+  }
+
+  adults = {
+    'male': {'survived':[], 'deceased':[]},
+    'female': {'survived':[], 'deceased':[]}
+  }
+
+  elderly = {
+    'male': {'survived':[], 'deceased':[]},
+    'female': {'survived':[], 'deceased':[]}
+  }
+
+  for index in range(len(data['age'])):
+    age = data['age'][index]
+
+    # children
+    if (age < 18):
+      if (data['sex'][index] == 'male' and data['survived'][index]):
+        children['male']['survived'].append(age)
+      elif (data['sex'][index] == 'male' and not data['survived'][index]):
+        children['male']['deceased'].append(age)
+      elif (data['sex'][index] == 'female' and data['survived'][index]):
+        children['female']['survived'].append(age)
+      elif (data['sex'][index] == 'female' and not data['survived'][index]):
+        children['female']['deceased'].append(age)
+
+    # adults
+    elif (age < 65):
+      if (data['sex'][index] == 'male' and data['survived'][index]):
+        adults['male']['survived'].append(age)
+      elif (data['sex'][index] == 'male' and not data['survived'][index]):
+        adults['male']['deceased'].append(age)
+      elif (data['sex'][index] == 'female' and data['survived'][index]):
+        adults['female']['survived'].append(age)
+      elif (data['sex'][index] == 'female' and not data['survived'][index]):
+        adults['female']['deceased'].append(age)
+
+    # elderly
+    else:
+      if (data['sex'][index] == 'male' and data['survived'][index]):
+        elderly['male']['survived'].append(age)
+      elif (data['sex'][index] == 'male' and not data['survived'][index]):
+        elderly['male']['deceased'].append(age)
+      elif (data['sex'][index] == 'female' and data['survived'][index]):
+        elderly['female']['survived'].append(age)
+      elif (data['sex'][index] == 'female' and not data['survived'][index]):
+        elderly['female']['deceased'].append(age)
+
+  x_labels = ['children', 'adults', 'elderly']
+  male_survivors = [len(children['male']['survived']), len(adults['male']['survived']), len(elderly['male']['survived'])]
+  female_survivors = [len(children['female']['survived']), len(adults['female']['survived']), len(elderly['female']['survived'])]
+
+  ax.plot(x_labels, male_survivors, label='Male Survivors')
+  ax.plot(x_labels, female_survivors, label='Female Survivors')
+  ax.set_title('Age, Sex vs Survival')
+  ax.legend()
+  ax.set_title('Fare vs Survival')
+
+
 def run():
-  # The function should start by calling the first function and storing the resulting dictionary in a local variable named data.
   data = read_data()
 
-  # initialise vars for dynamic plot creation according to number of keys
-  #plot = 0
-  numberOfPlots = len(data)
+  # plots arranged in a 2 by 2 grid
+  fig, axs = plt.subplots(2, 2)
 
-  # The function should then produce 4 subplots using Matplotlib. Each subplot should show any two of data sets (e.g. age and survived) plotted against one another using a suitable method (e.g. plot).  You are free to pick any visualisation method you wish but they must be appropriate to the data set and the insights you wish to make.
-  fig, axs = plt.subplots(numberOfPlots, 1) # (rows, cols, params)
-  fig.suptitle('Titanic Passenger Data')
-
-  # loop through keys in dictionary (data) and create graph with labels
-  #for key in data:
-  #  axs[plot].plot(data[key])
-  #  axs[plot].set_xlabel('Type')
-  #  axs[plot].set_ylabel('Number')
-  #  axs[plot].set_title(key)
-  #  plot += 1
-
-  # age vs. survived - bar - m/f-sur // m/f-notsur
-  axs[0].scatter(data['Age'], data['Age']) 
-  axs[0].plot(data['Survived']) 
-  axs[0].set_ylabel('Age')
-  axs[0].set_xlabel('Number')
-  axs[0].set_title('Survival by Age')
-
-  # age vs. survived
-  #axs[1].plot(dataAge, dataSurvived) 
-  #axs[1].set_xlabel('Survived')
-  #axs[1].set_ylabel('Sex')
-  #axs[1].set_title('Survival per Age')
-
-  # fare vs. survived
-
-
-  # all in one chart
-
-
+  # display plots with fixed locations on axs
+  plot_age_vs_survival(axs[0, 0], data)
+  plot_fare_vs_survival(axs[0, 1], data)
+  plot_sex_vs_survival(axs[1, 0], data)
+  plot_all_vs_survival(axs[1, 1], data)
+  
   plt.tight_layout() # makes sure the subplots don't overlap
   plt.show() # Displays the graphs
 
