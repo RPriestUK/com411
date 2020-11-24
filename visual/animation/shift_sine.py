f39fe5e7f89ea140bb5a96ac5cd88ba6eda45c75
repl.it user <replituser@example.com>
2020-11-24 11:41:42 +0000

    2411 - Visualisation - 28 Act 4 - Shifting Sine Wave Animation

--- visual/animation/shift_sine.py
+++ visual/animation/shift_sine.py
@@ -2,18 +2,12 @@
 # The program should consist of the following two functions:
 
 # The first function should be named animate and should have 1 parameter that represents the current frame number
-# The function should set the x limit for the axes to a minimum of 0 and a maximum of 720.
-# The function should then set the y limit for the axes to a minimum of -1 and a maximum of 1.
-# The function should then create a NumPy array named x with a range from 0 to the current frame number.
-# The function should then calculate the y values by multiplying the x values by PI/180
-# Finally, the function should plot the points given be x and y.
-
+# The function should display a sine wave that shifts each time a new frame is rendered.
 # The second function should be named run and should have no parameters. 
 # The function should create an animation with 720 frames and an interval of 100.
 # Finally, the function should show the plot.
 
-# What interesting observations can be made?
-# Code your solution (visual/animations/sine.py) and be sure to include appropriate comments in your code.
+# Code your solution (visual/animations/shift_sine.py) and be sure to include appropriate comments in your code.
 
 import matplotlib.pyplot as plt 
 import matplotlib.animation as animation 
@@ -24,21 +18,24 @@ fig, ax = plt.subplots()
 def animate(frame):
   # retrieve value of pi for creating y value
   pi = np.pi
+
+  ax.cla()
   # declare max sizes to fix figure size from start, y requires to be a minus value if x is positive/0
-  ax.set_xlim(0,720)
-  ax.set_ylim(-1,1)
+  ax.set_xlim(0,pi)
+  ax.set_ylim(-1.5,1.5)
   
-  # create x and y values for sine wave (x * 180 divided by Pi)
-  x = np.array([0, frame])
-  y = np.sin(x * (pi/180))
+  # create x and y values for sine wave (x * pi divided 180)
+  x = np.arange(0, 2*pi, 0.01)
+  y = np.sin(x + frame / 50)
 
   # plot each frame's x & y, with solid black line 
-  ax.plot(x, y, 'b-o', linewidth=0, markersize=2) 
+  ax.plot(x, y, 'b-o', linewidth=1, markersize=1) 
 
 def run():
   # figure to draw to, function to call for animation, number of frames (range, i.e. upto i), interval between frames, repetition - needs to be assigned to a variable
-  sine_animation = animation.FuncAnimation(fig, animate, frames = 720, interval = 100, repeat = False) 
+  sine_animation = animation.FuncAnimation(fig, animate, frames = 720, interval = 100) 
 
   plt.show()
 
 run()
+
