76f9b4a0751662970072eb6d0f6f401a219b3bb7
repl.it user <replituser@example.com>
2020-11-24 09:51:32 +0000

    2411 - Visualisation - Act 28 - Simple Animation with FigClearing

---
+++ visual/animation/marker.py
@@ -0,0 +1,21 @@
+import matplotlib.pyplot as plt 
+import matplotlib.animation as animation 
+
+fig, ax = plt.subplots()
+
+def animate(frame):
+  # clear axs (in this case - before each frame is drawn)
+  ax.cla()
+  # declare max sizes to fix figure size from start, or omit to resize figure's axis to the data as it is populated
+  ax.set_xlim(0,10)
+  ax.set_ylim(0,10)
+  # plot each frame's x & y, with red O markers
+  ax.plot(frame, frame, 'ro') 
+
+def run():
+  # figure to draw to, function to call for animation, number of frames (range, i.e. upto i), interval between frames, repetition - needs to be assigned to a variable
+  basic_animation = animation.FuncAnimation(fig, animate, frames = 10, interval = 1000, repeat = False) 
+
+  plt.show()
+
+run()
