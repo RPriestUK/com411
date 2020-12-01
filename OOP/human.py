3fe32ac55e81735665dd9b7988b7ec6d109a83f2
repl.it user <replituser@example.com>
2020-12-01 10:28:51 +0000

    0112 - 30 - OOP - Act 2 - repr and str outputsextended

--- OOP/human.py
+++ OOP/human.py
@@ -53,7 +53,7 @@ class Robot:
 
   # str string for user friendly output
   def __str__(self):
-    return f'My name is {self.name} and I am {self.age} years old.'
+    return f'My name is {self.name} and I am {self.age} years old, and I am a Robot.'
 
 class Human:
 
@@ -72,10 +72,22 @@ class Human:
   def display(self):
     print(f"I am {self.name}")
 
+  # repr string, used for debugging
+  def __repr__(self):
+    return f'human(name={self.name}, age={self.age})'
+
+  # str string for user friendly output
+  def __str__(self):
+    return f'My name is {self.name} and I am {self.age} years old, and I am a Human.'
+
 # default invisible name for program
 if (__name__ == "__main__"):
   human = Human()
-  human.display()
+  print(repr(human))
+  print() 
+  print(str(human))
   print()
   robot = Robot()
-  print(robot.__str__())
+  print(repr(robot))
+  print() 
+  print(str(robot))
