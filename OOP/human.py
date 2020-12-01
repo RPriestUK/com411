b735d95ff2bed2cb186af103632d56f6f9884ad2
repl.it user <replituser@example.com>
2020-12-01 10:25:36 +0000

    0112 - 30 - OOP - Act 2 - repr and str outputs

--- OOP/human.py
+++ OOP/human.py
@@ -27,24 +27,6 @@
 #   human = Human()
 #   human.display()class Robot:
 
-  # A class attribute
-#   laws = "Protect, Obey and Survive"
-
-  # A class method
-#   def the_laws():
-#     print(Robot.laws)
-
-  # An initialiser (special instance method)
-#   def __init__(self):
-
-    # An instance attribute
-#     self.name = "Robot"
-#     self.age = 0
-
-  # An instance method
-#   def display(self):
-#     print(f"I am {self.name}")
-
 class Robot:
 
   # A class attribute
@@ -64,6 +46,14 @@ class Robot:
   # An instance method
   def display(self):
     print(f"I am {self.name}")
+  
+  # repr string, used for debugging
+  def __repr__(self):
+    return f'robot(name={self.name}, age={self.age})'
+
+  # str string for user friendly output
+  def __str__(self):
+    return f'My name is {self.name} and I am {self.age} years old.'
 
 class Human:
 
@@ -82,6 +72,10 @@ class Human:
   def display(self):
     print(f"I am {self.name}")
 
+# default invisible name for program
 if (__name__ == "__main__"):
   human = Human()
   human.display()
+  print()
+  robot = Robot()
+  print(robot.__str__())
