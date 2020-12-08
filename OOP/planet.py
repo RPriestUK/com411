84e95d2bf12a28ed1dddf19c4a00b7d9f56912f6
repl.it user <replituser@example.com>
2020-12-08 11:19:00 +0000

    0812 - 31 - OOP - Sub Classes - Planets anets updated with inhabitants

--- OOP/planet.py
+++ OOP/planet.py
@@ -26,48 +26,25 @@ from robot import Robot
 from human import Human
 
 class Planet:
-  # The class should contain the following instance variables:
-  #   humans - a list of human objects (initially an empty # list)
-  #   robots - a list of robot objects (initially an empty list)
+  # generate empty list of inhabitants
+  inhabitants = []
+
+  # instance variables:
   def __init__(self):
-
-    self.inhabitants = {
-      'humans': [],
-      'robots': []
-    }
+    pass
+    
+  def add_inhabitant(self, inhabitant):
+    # adds entered inhabitant to list
+    self.inhabitants.append(inhabitant)
   
-  # The class should contain the following instance methods:
-
-  #   add_human - adds a human object to the list of humans
-  #   remove_human - removes a human object from the list of humans
-
-  #   add_robot - adds a robot object to the list of robots
-  #   remove_robot - removes a robot object from the list of robots
-
-  def add_human(self, human):
-    # adds entered inhabitant to humans list
-    self.inhabitants['humans'].append(human)
-  
-  def remove_human(self, human):
+  def remove_inhabitant(self, inhabitant):
     # checks the inhabitant is in the list and removes them, otherwise displays error
-    if (self.inhabitants['humans'].count(human) <= 0):
-      print("The inhabitant '{}' is not in the 'Humans' list".format(human))
-      print() 
+    if (inhabitant in self.inhabitants):
+      self.inhabitants.remove(inhabitant)
     else:
-      self.inhabitants['humans'].remove(human)
-  
-  def add_robot(self, robot):
-    # adds entered inhabitant to robots list
-    self.inhabitants['robots'].append(robot)
-  
-  def remove_robot(self, robot):
-    # checks the inhabitant is in the list and removes them, otherwise displays error
-    if (self.inhabitants['robots'].count(robot) <= 0):
-      print("The inhabitant '{}' is not in the 'Robots' list".format(robot))
+      print("The inhabitant '{}' is not in the 'Inhabitants' list".format(inhabitant))
       print() 
-    else:
-      self.inhabitants['robots'].remove(robot)
-  
+    
   # The class should contain the following magic methods:
 
   #   __repr__ - returns a string containing a formal representation of a planet object (includes the list of humans and the list of robots)
@@ -80,7 +57,7 @@ class Planet:
 
   # str string for user friendly output
   def __str__(self):
-    return f"The Planet contains 1 dictionary of 2 lists; Humans: {self.inhabitants['humans']} & Robots:{self.inhabitants['robots']}."
+    return f"The Planet contains a list of inhabitants; Humans & Robots: {self.inhabitants}."
 
 # You should add suitable code to create and test your class Planet.
 if (__name__ == "__main__"):
@@ -96,21 +73,21 @@ if (__name__ == "__main__"):
   bob = Human("Bob", 5)
   dick = Human("Dick", 15)
   harry = Human("Harry", 98)  
-  planet.add_human(bob)
-  planet.add_human(dick)
-  planet.add_human(harry)
+  planet.add_inhabitant(bob)
+  planet.add_inhabitant(dick)
+  planet.add_inhabitant(harry)
   # add to robot list to populate
   beep = Robot("Beep")
   boop = Robot("Boop", 6)
   walle = Robot("Wall-e", 3)
-  planet.add_robot(beep)
-  planet.add_robot(boop)
-  planet.add_robot(walle)
-  # remove from human list to confirm
-  planet.remove_human("Dick")
-  planet.remove_human("Dick")
-  # remove from robot list to confirm
-  planet.remove_robot("Boop")
+  planet.add_inhabitant(beep)
+  planet.add_inhabitant(boop)
+  planet.add_inhabitant(walle)
+  # remove from list to confirm
+  planet.remove_inhabitant(dick)
+  planet.remove_inhabitant(dick)
+  # remove from list to confirm
+  planet.remove_inhabitant(boop)
   # display user friendly output to show populated lists:
   print(str(planet))
   print() 
